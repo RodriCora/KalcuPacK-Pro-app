b2789cb31990e9d1ea71783d0975f6088d06a123
RodriCora <rodrigoocorazzaa@gmail.com>
2026-05-14 21:24:10 -0300

    commi3

--- sw.js
+++ sw.js
@@ -1,22 +1,26 @@
-const CACHE_NAME = "kalcupack-v3";
+const CACHE_NAME = "kalcupack-v4";
 const urlsToCache = [
   "./",
-  "index.html",
-  "style.css",
-  "app.js",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
